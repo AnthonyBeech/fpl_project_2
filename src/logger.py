89bd8e1c7080ceaa1b657d992b4ff93009904294
AnthonyBeech <abeech123@virginmedia.com>
2024-01-07 14:34:03 +0000

    -

--- src/logger.py
+++ src/logger.py
@@ -5,25 +5,24 @@ from datetime import datetime
 from src.utils import load_config
 
 cfg = load_config()
-logs_dir = cfg["logs_dir"]
+log_folder = cfg["logs_dir"]
 
 
-def setup_logger(logs_dir):
-    # create dir to store log files
-    os.makedirs(logs_dir, exist_ok=True)
+# create dir to store log files
+logs_dir = os.path.join(os.getcwd(), log_folder)
+os.makedirs(logs_dir, exist_ok=True)
 
-    # Define log file name with current timestamp
-    log_file_name = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
-    log_file_path = os.path.join(logs_dir, log_file_name)
-
-    # Configure logging
-    logging.basicConfig(
-        filename=log_file_path,
-        format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-        level=logging.INFO,
-    )
-
-    logging.info("Initializing logger...")
+# Define log file name with current timestamp
+log_file_name = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+log_file_path = os.path.join(logs_dir, log_file_name)
 
 
-setup_logger(logs_dir)
+# Configure logging
+logging.basicConfig(
+    filename=log_file_path,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+# Test logging
+logging.info("Logger initialized.")
---
+++ tests/test_src/test_logger.py
@@ -0,0 +1,10 @@
+from src.logger import logging
+
+
+def test_logger_setup():
+    logging.info("")
+    
+    # logging within pytest is not possible without major effort.
+    # No testing carried out
+    
+    assert True
