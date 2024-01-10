768eaa1e2beab03abca2713c93961d7050cf7af1
AnthonyBeech <abeech123@virginmedia.com>
2024-01-10 22:38:50 +0000

    -

--- tests/test_src/test_components/test_data_transformation.py
+++ tests/test_src/test_components/test_data_transformation.py
@@ -2,20 +2,41 @@ import os
 
 from src.components.data_transformation import DataCleaner
 
-latest_dir = "tests/test_data/latest"
-transformed_dir = "tests/test_data/transformed"
-overlap = 1
-keep_headers = [
-    'assists', 'bonus', 'bps', 'clean_sheets', 'creativity', 
-    'goals_conceded', 'goals_scored', 'ict_index', 'influence', 
-    'kickoff_time', 'minutes', 'own_goals', 'penalties_missed', 
-    'penalties_saved', 'red_cards', 'saves', 'team_a_score', 
-    'team_h_score', 'threat', 'value', 'was_home', 'yellow_cards', 
-    'starts', 'position'
-]
 
+def test_clean_data():
+    latest_dir = "tests/test_data/latest"
+    transformed_dir = "tests/test_data/transformed"
+    overlap = 1
+    keep_headers = [
+        "assists",
+        "bonus",
+        "bps",
+        "clean_sheets",
+        "creativity",
+        "goals_conceded",
+        "goals_scored",
+        "ict_index",
+        "influence",
+        "kickoff_time",
+        "minutes",
+        "own_goals",
+        "penalties_missed",
+        "penalties_saved",
+        "red_cards",
+        "saves",
+        "team_a_score",
+        "team_h_score",
+        "threat",
+        "value",
+        "was_home",
+        "yellow_cards",
+        "starts",
+        "position",
+    ]
 
-transformer = DataCleaner(latest_dir, transformed_dir, overlap, keep_headers)
+    transformer = DataCleaner(latest_dir, transformed_dir, overlap, keep_headers)
 
-for csv_path in os.listdir(latest_dir):
-    transformer.clean(csv_path)
+    for csv_path in os.listdir(latest_dir):
+        transformer.clean(csv_path)
+        
+    assert True
